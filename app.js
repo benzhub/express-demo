799a3e4b6b4e37ba9f953e4abbaf936638ce0153
lenrich <lenrich1403@gmail.com>
2022-07-19 23:07:54 +0800

    eslint && prettier setting

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
-const express = require('express')
-const morgan = require('morgan')
-const tourRouter = require('./routes/tourRoutes')
-const userRouter = require('./routes/userRoutes')
-const app = express()
+const express = require('express');
+const morgan = require('morgan');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
+const app = express();
 
 // 1. middleware
-console.log(process.env.NODE_ENV)
+console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'))
+    app.use(morgan('dev'));
 }
 //express.json is middleware
-app.use(express.json())
-app.use(express.static(`${__dirname}/public`))
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
     console.log('Hello from the middleware');
     next();
-})
+});
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     next();
-})
+});
 
 // 3.Routes
 // 實際上tourRouter, userRouter是中間件
