50ef927670a4ea5a96c046e4b82ab9e2ac14ae18
lenrich <lenrich1403@gmail.com>
2022-07-20 11:37:51 +0800

    creating documents and testing the model

--- server.js
+++ server.js
@@ -4,18 +4,52 @@ const mongoose = require('mongoose');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
+
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+console.log(DB)
 mongoose
     // .connect(process.env.DATABASE_LOCAL, {
+    // no longer supported options in Mongoose 6
     .connect(DB, {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useFindAndModify: false
+        // useNewUrlParser: true,
+        // useCreateIndex: true,
+        // useFindAndModify: false
     }).then(() => console.log('DB connection successful'));
+
+const tourSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'A tour must have a name'],
+        unique: true
+    },
+    rating: {
+        type: Number,
+        default: 4.5
+    },
+    price: {
+        type: Number,
+        required: [true, 'A tour must have a price']
+    }
+});
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+    name: 'The Park Camper',
+    price: 599
+})
+
+testTour
+    .save()
+    .then(doc => {
+        console.log(doc)
+    })
+    .catch(err => {
+        console.log('Error:', err)
+    })
 
 // console.log(process.env)
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
     console.log(`App running on port ${port}...`);
 });
