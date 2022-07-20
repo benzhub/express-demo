eacf2a0fb5f82238c28cd1087f5772bb275b2657
lenrich <lenrich1403@gmail.com>
2022-07-20 08:05:22 +0800

    connect to atlas mongo from express app

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
-const dotenv = require('dotenv')
-dotenv.config({ path: './config.env' })
-const app = require('./app')
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+dotenv.config({ path: './config.env' });
+const app = require('./app');
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+mongoose
+    // .connect(process.env.DATABASE_LOCAL, {
+    .connect(DB, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }).then(() => console.log('DB connection successful'));
 
 // console.log(process.env)
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
-    console.log(`App running on port ${port}...`)
-})
+    console.log(`App running on port ${port}...`);
+});
