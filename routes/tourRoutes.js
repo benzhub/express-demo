9eb2afd42fd282949cd8e5a55c17779d37fa04ef
lenrich <lenrich1403@gmail.com>
2022-11-02 22:17:55 +0800

    Making the API Better Aliasing

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -6,6 +6,12 @@ const router = express.Router();
 // middleware
 // router.param('id', tourController.checkID)
 
+// Alias Router
+// request => http://localhost:8000/api/v1/tours/top-5-cheap
+router
+    .route('/top-5-cheap')
+    .get(tourController.aliasTopTours, tourController.getAllTours)
+
 router
     .route('/')
     .get(tourController.getAllTours)
